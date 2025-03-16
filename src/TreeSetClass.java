3305c0e86671f8fa6d6d0bfddcbbb0f3ba93f4f7
rakx07 <rakib15-13846@diu.edu.bd>
2025-03-16 23:01:01 +0600

    TreeSet functionalities are added with their time complexity and working procedure.

---
+++ src/TreeSetClass.java
@@ -0,0 +1,69 @@
+import java.util.List;
+import java.util.TreeSet;
+
+public class TreeSetClass {
+    public static void main(String[] args) {
+        TreeSet<Integer> treeSet = new TreeSet<>();
+
+        /*
+         * Add operation adds elements to the hash set.
+         * Maintain insertion order.
+         * Elements are sorted upon insertion.
+         * Time complexity - O(log(n))
+         */
+        treeSet.add(5);
+        treeSet.add(2);
+        treeSet.add(3);
+        treeSet.add(4);
+        treeSet.add(1);
+
+        printSet(treeSet);
+
+        /*
+         * It returns the size of the tree set.
+         * Return type is always integer.
+         * Time complexity - O(1)
+         */
+        int size = treeSet.size();
+        System.out.println("\n\nThe size of the TreeSet is " + size);
+
+        /*
+         * Remove operation removes elements from the tree set.
+         * Time complexity - O(log(n))
+         */
+        treeSet.remove(3);
+        treeSet.remove(1);
+
+        printSet(treeSet);
+
+        /*
+         * Checks if the tree set contains particular element
+         * Time complexity - O(log(n))
+         */
+        boolean contains = treeSet.contains(2);
+        System.out.println("\n\nThe result of contain is - " + contains);
+
+        /*
+         * Checks if the tree set contains all the mentioned elements
+         * Time complexity - O(n log(n))
+         */
+        boolean containsAll = treeSet.containsAll(List.of(1, 2));
+        System.out.println("\n\nThe result of contain is - " + containsAll);
+
+        /*
+         * Clear operation removes all the elements from the tree set.
+         * Time complexity - O(n log(n))
+         */
+        treeSet.clear();
+
+        printSet(treeSet);
+
+    }
+    private static void printSet(TreeSet<Integer> treeSet){
+        System.out.println("\nPrinting the set");
+
+        for(Integer element: treeSet){
+            System.out.println(element);
+        }
+    }
+}
